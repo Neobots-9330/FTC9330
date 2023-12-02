1c401be7f55efb13d3f7635fca699fa0a473f234
Colton Badock <816394@stu.asd16.org>
2023-12-02 10:20:42 -0800

    Started branch for programming launcher and hanger

---
+++ TeleOp1.java
@@ -0,0 +1,89 @@
+package org.neobots2903.ftcCenterstage2023;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class TeleOp1 extends LinearOpMode {
+    public boolean speedUpPressed = false;
+    public boolean speedDownPressed = false;
+    public boolean wheeling = false;
+    
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot9330 robot = new Robot9330(this, false);
+        
+        int maxSpeedMultiplier = 3;
+        int speedMultiplier = (int) Math.ceil(maxSpeedMultiplier / 2.0);
+
+        waitForStart();
+        
+        if(isStopRequested()) return;
+
+        while(opModeIsActive()) {
+            //change speedMultiplier
+            if(gamepad1.right_bumper && speedMultiplier < maxSpeedMultiplier && !speedUpPressed) {
+                speedMultiplier++;
+                speedUpPressed = true;
+                
+                new Thread() {
+                    @Override
+                    public void run() {
+                        robot.pause(0.25);
+                        speedUpPressed = false;
+                    }
+                }.start();
+            }
+            
+            if(gamepad1.left_bumper && speedMultiplier > 1 && !speedDownPressed) {
+                speedMultiplier--;
+                speedDownPressed = true;
+                
+                new Thread() {
+                    @Override
+                    public void run() {
+                        robot.pause(0.25);
+                        speedDownPressed = false;
+                    }
+                }.start();
+            }
+            
+            if(gamepad1.x) robot.imu.initialize(robot.parameters);
+            
+            double x = gamepad1.left_stick_x;
+            double y = -gamepad1.left_stick_y;
+            double rx = gamepad1.right_stick_x;
+
+            robot.move(x * speedMultiplier / maxSpeedMultiplier, y * speedMultiplier / maxSpeedMultiplier, rx * speedMultiplier / maxSpeedMultiplier);
+            
+            /*if(gamepad1.a && !wheeling) {
+                wheeling = true;
+                
+                new Thread() {
+                    @Override
+                    public void run() {
+                        robot.servoWheelie.setPosition(1);
+                        robot.pause(1);
+                        robot.servoWheelie.setPosition(0);
+                        wheeling = false;
+                    }
+                }.start();
+            }*/
+            
+            //When "y" is pressed on gamepad2, launch the airplane.
+            if (gamepad2.y) {
+                robot.launchAirplane();
+            }
+            
+            //If "b" is pressed on gamepad2, toggle the pixel trap up or down.
+            if (gamepad2.b) {
+                robot.togglePixelTrap();
+                sleep(250);
+            }
+        }
+    }
+}
---
+++ robot9330.java
@@ -0,0 +1,443 @@
+package org.neobots2903.ftcCenterstage2023;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class Robot9330 {
+    public BNO055IMU imu;
+    public BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+    public DcMotor motorDriveFrontLeft; //Front Left motor for wheel base.
+    public DcMotor motorDriveFrontRight; //Front Right motor for wheel base.
+    public DcMotor motorDriveBackLeft; //Rear Left motor for wheel base.
+    public DcMotor motorDriveBackRight; //Rear Right motor for wheel base.
+    public OpMode opMode;
+    //public Servo servoWheelie;
+    public boolean flip;
+    public Servo airplaneLauncherRelease; //The servo that release the airplane on the launcher.
+    boolean planeIsLaunched = false; //Switched to true when the plane is launched.
+    public Servo pixelTrapServoOne; //First Servo for the pixel trap.
+    public Servo pixelTrapServoTwo; //Second Servo for the pixel trap.
+    boolean pixelTrapIsDown = false;
+    public DcMotor motorHangShoulder;
+    public DcMotor motorHangArm;
+    
+    //Reverse motors for atonnymus
+    boolean atonnymus_motorDriveFrontLeft_reverse = false;
+    boolean atonnymus_motorDriveFrontRight_reverse = true;
+    boolean atonnymus_motorDriveBackLeft_reverse = true;
+    boolean atonnymus_motorDriveBackRight_reverse = false;
+    
+    //Auto variables are all in Inches.
+    double autoForward = 30.75; //Distance to move forward toward strip for all autos. //Others: 32.75
+    double autoBackward = 27.75; //Distance to move backward away from strip for all autos, alligning between rigging to starfe for backdrop park.//Previous 28.75
+    double autoWing = 98 + 2; //Distance to strafe from the wing to backdrop park.
+    double autoBackdrop = 50.5; //Distance to strafe from the Backdrop start to backdrop park.
+    double autoPower = 0.2;
+    
+    public Robot9330(OpMode opMode, boolean flip) {
+        this.opMode = opMode;
+        this.flip = flip;
+        imu = opMode.hardwareMap.get(BNO055IMU.class, "imu");
+        motorDriveFrontLeft = opMode.hardwareMap.get(DcMotor.class, "motorDriveFrontLeft");
+        motorDriveFrontRight = opMode.hardwareMap.get(DcMotor.class, "motorDriveFrontRight");
+        motorDriveBackLeft = opMode.hardwareMap.get(DcMotor.class, "motorDriveBackLeft");
+        motorDriveBackRight = opMode.hardwareMap.get(DcMotor.class, "motorDriveBackRight");
+        //servoWheelie = opMode.hardwareMap.get(Servo.class, "servoWheelie");
+        airplaneLauncherRelease = opMode.hardwareMap.get(Servo.class, "servoDrone");
+        pixelTrapServoOne = opMode.hardwareMap.get(Servo.class, "pixelTrapServoOne"); //left servo.
+        pixelTrapServoTwo = opMode.hardwareMap.get(Servo.class, "pixelTrapServoTwo"); //right servo
+        motorHangShoulder = opMode.hardwareMap.get(DcMotor.class, "motorHangShoulder");
+        motorHangArm = opMode.hardwareMap.get(DcMotor.class, "motorHangArm");
+        //airplaneLauncherRelease.setPosition(0.64); //Lock in the rubber band; Removed, moves the servo to much.
+        
+        //Set up IMU
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        imu.initialize(parameters);
+        
+        /*
+        
+        WARNING: LAST YEARS HALF-ASSED CODE, IF MOTORS NEED TO BE REVERSED, REVERSE IT HERE!
+        
+        */
+        // Reverse any motors if necessary
+        //motorDriveFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorDriveFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorDriveBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorDriveBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        // motorLiftRight.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    
+    // Moves robot
+    public void move(double x, double y, double rx) {
+        if(flip) {
+            x = -x;
+            rx = -rx;
+        }
+        
+        // Magical math
+        double botHeading = -imu.getAngularOrientation().firstAngle;
+        double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
+        double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (rotY + rotX + rx) / denominator;
+        double frontRightPower = -(rotY - rotX - rx) / denominator; //MOTOR is flipped.
+        double backLeftPower = -(rotY - rotX + rx) / denominator;
+        double backRightPower = (rotY + rotX - rx) / denominator;
+        
+        // Move motors
+        motorDriveFrontLeft.setPower(frontLeftPower);
+        motorDriveFrontRight.setPower(frontRightPower);
+        motorDriveBackLeft.setPower(backLeftPower);
+        motorDriveBackRight.setPower(backRightPower);
+    }
+    
+    // Moves robot for an amount of time
+    public void move(double x, double y, double rx, double seconds) {
+        move(x, y, rx);
+        pause(seconds);
+        move(0, 0, 0);
+    }
+    
+    public void pause(double seconds) {
+        try {
+            Thread.sleep((long) (seconds * 1000));
+        } catch(InterruptedException exc) {}
+    }
+    
+    //Rotates a servo, releasing the paper airplane.
+    public void launchAirplane() {
+        if (planeIsLaunched == false) { //Servo can only be rotated once.
+            airplaneLauncherRelease.setPosition(0); //NOTES: 0.64 to hold down band.
+            planeIsLaunched = true;
+            pause(1);
+            airplaneLauncherRelease.setPosition(1);
+            planeIsLaunched = false;
+        }
+    }
+
+    //Moves the pixel trap up if down, and downif up.
+    public void togglePixelTrap() {
+        if (pixelTrapIsDown == false) {
+            pixelTrapServoOne.setPosition(1);
+            pixelTrapServoTwo.setPosition(1);
+            pixelTrapIsDown = true;
+        } else if (pixelTrapIsDown == true) {
+            pixelTrapServoOne.setPosition(0);
+            pixelTrapServoTwo.setPosition(0);
+            pixelTrapIsDown = false;
+        }
+    }
+    
+    //Tick is what the motor encoders return.
+    //One wheel rotation = 280 ticks = 23.93 centimeters.
+    //Converts inch to ticks for driving forward and back.
+    public int toTicks_ForwardAndBack(double inch) {
+        return (int) Math.ceil(inch * 29.71);
+    }
+    
+    //Converts inch to ticks for driving left and right (Strafe).
+    public int toTicks_LeftAndRight(double inch) {
+        return (int) Math.ceil(inch * 30);
+    }
+    
+    //Fully resets encoders.
+    public void runBackEncoders() {
+        motorDriveFrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorDriveFrontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorDriveBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorDriveBackRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    
+    
+    //Moves robot forward for a specified number of ticks; power is the motor power.
+    //Direction id: 0 = forward; 1 = backward;
+    public void moveForwardOrReverse(int ticks, double power, int direction_id) {
+        runBackEncoders(); //Reset encoders.
+        
+        //Move forward.
+        if (direction_id == 0) {
+            //Set motor target positions to forward.
+            
+            if (atonnymus_motorDriveBackRight_reverse == false) {
+                motorDriveBackRight.setTargetPosition(ticks);
+                motorDriveBackRight.setPower(power); //Werid motor.
+            } else {
+                motorDriveBackRight.setTargetPosition(-ticks);
+                motorDriveBackRight.setPower(-power); //Werid motor.
+            }
+            
+            if (atonnymus_motorDriveBackLeft_reverse == false) {
+                motorDriveBackLeft.setTargetPosition(ticks);
+                motorDriveBackLeft.setPower(power);
+            } else {
+                motorDriveBackLeft.setTargetPosition(-ticks);
+                motorDriveBackLeft.setPower(-power);
+            }
+            
+            if (atonnymus_motorDriveFrontRight_reverse == false) {
+                motorDriveFrontRight.setTargetPosition(ticks);
+                motorDriveFrontRight.setPower(power);
+            } else {
+                motorDriveFrontRight.setTargetPosition(-ticks);
+                motorDriveFrontRight.setPower(-power);
+            }
+            
+            if (atonnymus_motorDriveFrontLeft_reverse == false) {
+                motorDriveFrontLeft.setTargetPosition(ticks);
+                motorDriveFrontLeft.setPower(power);
+            } else {
+                motorDriveFrontLeft.setTargetPosition(-ticks);
+                motorDriveFrontLeft.setPower(-power);
+            }
+            
+        }
+        
+        //Move backward
+        if (direction_id == 1) {
+            
+            if (atonnymus_motorDriveBackRight_reverse == false) {
+                motorDriveBackRight.setTargetPosition(-ticks);
+                motorDriveBackRight.setPower(-power); //Werid motor.
+            } else {
+                motorDriveBackRight.setTargetPosition(ticks);
+                motorDriveBackRight.setPower(power); //Werid motor.
+            }
+            
+            if (atonnymus_motorDriveBackLeft_reverse == false) {
+                motorDriveBackLeft.setTargetPosition(-ticks);
+                motorDriveBackLeft.setPower(-power);
+            } else {
+                motorDriveBackLeft.setTargetPosition(ticks);
+                motorDriveBackLeft.setPower(power);
+            }
+            
+            if (atonnymus_motorDriveFrontRight_reverse == false) {
+                motorDriveFrontRight.setTargetPosition(-ticks);
+                motorDriveFrontRight.setPower(-power);
+            } else {
+                motorDriveFrontRight.setTargetPosition(ticks);
+                motorDriveFrontRight.setPower(power);
+            }
+            
+            if (atonnymus_motorDriveFrontLeft_reverse == false) {
+                motorDriveFrontLeft.setTargetPosition(-ticks);
+                motorDriveFrontLeft.setPower(-power);
+            } else {
+                motorDriveFrontLeft.setTargetPosition(ticks);
+                motorDriveFrontLeft.setPower(power);
+            }
+        }
+        
+        motorDriveFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION); //Drive the motor to the encoder position.
+        motorDriveFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorDriveBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorDriveBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        
+    }
+    
+    
+    //One inch = 32 ticks.
+    //Strafe Right: Front left > Forwards; Rear Left > Backwards; Front right > backwards; rear right > forwards.
+    //Strafe Left: Front left > Backwards; Rear left > Forwards; Front Right > Forwards; Rear right > backwards.
+    //Strafe the robot sideways for a number of ticks.
+    //"direction_id"; 0 == left, 1 == right;
+    public void strafe(int ticks, double power, int direction_id) {
+        runBackEncoders(); //Set encoders to 0.
+        
+        //Strafe left
+        if (direction_id == 0) {
+            //Set motor target positions for Strafing left.
+            
+            if (atonnymus_motorDriveFrontLeft_reverse == false) {
+                motorDriveFrontLeft.setTargetPosition(-(ticks));
+                motorDriveFrontLeft.setPower(-power);
+            } else {
+                motorDriveFrontLeft.setTargetPosition(ticks);
+                motorDriveFrontLeft.setPower(power);
+            }
+            
+            if (atonnymus_motorDriveFrontRight_reverse == false) {
+                motorDriveFrontRight.setTargetPosition(ticks);
+                motorDriveFrontRight.setPower(power);
+            } else {
+                motorDriveFrontRight.setTargetPosition(-ticks);
+                motorDriveFrontRight.setPower(-power);
+            }
+            
+            if (atonnymus_motorDriveBackLeft_reverse == false) {
+                motorDriveBackLeft.setTargetPosition(ticks);
+                motorDriveBackLeft.setPower(power);
+            } else {
+                motorDriveBackLeft.setTargetPosition(-ticks);
+                motorDriveBackLeft.setPower(-power);
+            }
+            
+            if (atonnymus_motorDriveBackRight_reverse == false) {
+                motorDriveBackRight.setTargetPosition(-(ticks));
+                motorDriveBackRight.setPower(-power);
+            } else {
+                motorDriveBackRight.setTargetPosition(ticks);
+                motorDriveBackRight.setPower(power);
+            }
+            
+            motorDriveFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            
+        }
+    
+        //Strafe Right
+        if (direction_id == 1) {
+            
+            if (atonnymus_motorDriveFrontLeft_reverse == false) {
+                motorDriveFrontLeft.setTargetPosition(ticks);
+                motorDriveFrontLeft.setPower(power);
+            } else {
+                motorDriveFrontLeft.setTargetPosition(-ticks);
+                motorDriveFrontLeft.setPower(-power);
+            }
+            
+            if (atonnymus_motorDriveFrontRight_reverse == false) {
+                motorDriveFrontRight.setTargetPosition(-ticks);
+                motorDriveFrontRight.setPower(-power);
+            } else {
+                motorDriveFrontRight.setTargetPosition(ticks);
+                motorDriveFrontRight.setPower(power);
+            }
+            
+            if (atonnymus_motorDriveBackLeft_reverse == false) {
+                motorDriveBackLeft.setTargetPosition(-ticks);
+                motorDriveBackLeft.setPower(-power);
+            } else {
+                motorDriveBackLeft.setTargetPosition(ticks);
+                motorDriveBackLeft.setPower(power);
+            }
+            
+            if (atonnymus_motorDriveBackRight_reverse == false) {
+                motorDriveBackRight.setTargetPosition(ticks);
+                motorDriveBackRight.setPower(power);
+            } else {
+                motorDriveBackRight.setTargetPosition(-ticks);
+                motorDriveBackRight.setPower(-power);
+            }
+            
+            motorDriveFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+        
+    }
+    
+    //Wait for "motorDriveFrontRight" (Front right motor) to reach its encoder before entering the next atonnymus stage.
+    public void waitForMotorEncoderToFinnish() {
+        while(motorDriveFrontRight.isBusy()) {
+            
+        }
+    }
+    
+    public void autoBB() {
+        
+                                                    //Distance \ power \ direction.
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoForward), autoPower, 0);
+        waitForMotorEncoderToFinnish();
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoBackward), autoPower, 1);
+        waitForMotorEncoderToFinnish();
+        strafe(toTicks_LeftAndRight(autoBackdrop), autoPower, 0);
+        waitForMotorEncoderToFinnish();
+        
+        
+    }
+    
+    
+    public void autoBW() {
+        
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoForward), autoPower, 0);
+        waitForMotorEncoderToFinnish();
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoBackward), autoPower, 1);
+        waitForMotorEncoderToFinnish();
+        strafe(toTicks_LeftAndRight(autoWing), autoPower, 0);
+        waitForMotorEncoderToFinnish();
+    }
+    
+    
+    
+    public void autoRB() {
+        
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoForward), autoPower, 0);
+        waitForMotorEncoderToFinnish();
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoBackward), autoPower, 1);
+        waitForMotorEncoderToFinnish();
+        strafe(toTicks_LeftAndRight(autoBackdrop), autoPower, 1);
+        waitForMotorEncoderToFinnish();
+    }
+    
+    
+    public void autoRW() {
+        
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoForward), autoPower, 0);
+        waitForMotorEncoderToFinnish();
+        moveForwardOrReverse(toTicks_ForwardAndBack(autoBackward), autoPower, 1);
+        waitForMotorEncoderToFinnish();
+        strafe(toTicks_LeftAndRight(autoWing), autoPower, 1);
+        waitForMotorEncoderToFinnish();
+        
+    }
+}
+
+
+/*
+Plane launcher notes:
+line 20, 21, 32, 83-89 commented out for now.
+*/
+
+
+/*//Set target positions to negative (backwards).
+            motorDriveFrontLeft.setTargetPosition(-(ticks)); //Set target position for all motors.
+            motorDriveFrontRight.setTargetPosition(-(ticks));
+            motorDriveBackLeft.setTargetPosition(-(ticks));
+            motorDriveBackRight.setTargetPosition(-(ticks));
+        
+            //Set motor power to reverse.
+            motorDriveFrontLeft.setPower(-power);
+            motorDriveFrontRight.setPower(-power);
+            motorDriveBackLeft.setPower(-power);
+            motorDriveBackRight.setPower(-power); //Werid motor.*/
+
+/*//Set motor target positions for Strafing left.
+            motorDriveFrontLeft.setTargetPosition(-(ticks));
+            motorDriveFrontRight.setTargetPosition(ticks);
+            motorDriveBackLeft.setTargetPosition(ticks);
+            motorDriveBackRight.setTargetPosition(-(ticks)); //Motor is possibly backwards
+            
+            motorDriveFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION); //Drive the motor to the encoder position.
+            motorDriveFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            
+            motorDriveFrontLeft.setPower(-power);
+            motorDriveFrontRight.setPower(power);
+            motorDriveBackLeft.setPower(power);
+            motorDriveBackRight.setPower(-power); //Motor is somehow backwards.*/
+            
+            
+            /*//Set motor target positions for Strafing left.
+            motorDriveFrontLeft.setTargetPosition(ticks);
+            motorDriveFrontRight.setTargetPosition(-(ticks));
+            motorDriveBackLeft.setTargetPosition(-(ticks));
+            motorDriveBackRight.setTargetPosition(ticks); //Motor is possibly backwards
+            
+            motorDriveFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION); //Drive the motor to the encoder position.
+            motorDriveFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorDriveBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            
+            motorDriveFrontLeft.setPower(power);
+            motorDriveFrontRight.setPower(-power);
+            motorDriveBackLeft.setPower(-power);
+            motorDriveBackRight.setPower(power); //Motor is somehow backwards.*/
