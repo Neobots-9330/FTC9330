43b9b48f51ec34e76c3a1d18554ab096b70f748a
Colton Badock <816394@stu.asd16.org>
2023-12-11 14:56:47 -0800

    Final code after Second Compititon.

--- robot9330.java
+++ robot9330.java
@@ -31,9 +31,15 @@ public class Robot9330 {
     boolean atonnymus_motorDriveBackLeft_reverse = true;
     boolean atonnymus_motorDriveBackRight_reverse = false;
     
+    //"Magic numbers"
+    int hanger_upright_position = 175; //Total ticks to raise the hanger arm to upright position.
+    int hanger_extender_position = 3450; //Total ticks to move the hanger actuator upward to grip rigging.
+    double ticksToMoveForwardOneInch = 29.71;
+    int ticksToStrafOneInch = 30;
+    
     //Auto variables are all in Inches.
     double autoForward = 30.75; //Distance to move forward toward strip for all autos. //Others: 32.75
-    double autoBackward = 27.75; //Distance to move backward away from strip for all autos, alligning between rigging to starfe for backdrop park.//Previous 28.75
+    double autoBackward = 26.75; //Distance to move backward away from strip for all autos, alligning between rigging to starfe for backdrop park.//Previous 28.75
     double autoWing = 98 + 2; //Distance to strafe from the wing to backdrop park.
     double autoBackdrop = 50.5; //Distance to strafe from the Backdrop start to backdrop park.
     double autoPower = 0.2;
@@ -58,11 +64,9 @@ public class Robot9330 {
         parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
         imu.initialize(parameters);
         
-        /*
+        //Set brake on hang arm.
+        motorHangArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         
-        WARNING: LAST YEARS HALF-ASSED CODE, IF MOTORS NEED TO BE REVERSED, REVERSE IT HERE!
-        
-        */
         // Reverse any motors if necessary
         //motorDriveFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         motorDriveFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -70,6 +74,59 @@ public class Robot9330 {
         motorDriveBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
         // motorLiftRight.setDirection(DcMotorSimple.Direction.REVERSE);
     }
+    
+    //Inailizes the hangers 0 tick position to its current starting position.
+    public void initHanger() {
+        motorHangShoulder.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    
+    //Iniatlises the hangers extension. Sets its encoder to 0.
+    public void initHangerExtender() {
+        motorHangArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+    
+    public int getHangerExtensionPosition() {
+        return motorHangArm.getCurrentPosition();
+    }
+    
+    //Raises the hanger to its upward position. 164 is upright.
+    public void raiseHanger() {
+        motorHangShoulder.setTargetPosition(hanger_upright_position);
+        motorHangShoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorHangShoulder.setPower(0.05);
+        
+        while (motorHangShoulder.isBusy()) {
+            
+        }
+        
+        motorHangShoulder.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    
+    public void lowerHanger() {
+        motorHangShoulder.setTargetPosition(0);
+        motorHangShoulder.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorHangShoulder.setPower(-0.05);
+    }
+    
+    //Extends hanger extender upward.//4189 is default
+    public void extendHangerExtender() {
+        motorHangArm.setTargetPosition(hanger_extender_position);
+        motorHangArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorHangArm.setPower(0.4);
+    }
+    
+    public void retractHangerExtender() {
+        motorHangArm.setTargetPosition(0);
+        motorHangArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorHangArm.setPower(-1);
+    }
+    
+    
+    //Returns the hangers position in ticks.
+    public int getHangerPosition() {
+        return motorHangShoulder.getCurrentPosition();
+    }
+    
     
     // Moves robot
     public void move(double x, double y, double rx) {
@@ -111,11 +168,8 @@ public class Robot9330 {
     //Rotates a servo, releasing the paper airplane.
     public void launchAirplane() {
         if (planeIsLaunched == false) { //Servo can only be rotated once.
-            airplaneLauncherRelease.setPosition(0); //NOTES: 0.64 to hold down band.
-            planeIsLaunched = true;
-            pause(1);
             airplaneLauncherRelease.setPosition(1);
-            planeIsLaunched = false;
+            planeIsLaunched = true;
         }
     }
 
@@ -136,12 +190,12 @@ public class Robot9330 {
     //One wheel rotation = 280 ticks = 23.93 centimeters.
     //Converts inch to ticks for driving forward and back.
     public int toTicks_ForwardAndBack(double inch) {
-        return (int) Math.ceil(inch * 29.71);
+        return (int) Math.ceil(inch * ticksToMoveForwardOneInch);
     }
     
     //Converts inch to ticks for driving left and right (Strafe).
     public int toTicks_LeftAndRight(double inch) {
-        return (int) Math.ceil(inch * 30);
+        return (int) Math.ceil(inch * ticksToStrafOneInch);
     }
     
     //Fully resets encoders.
